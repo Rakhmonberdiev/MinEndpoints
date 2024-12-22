ccc7cbccfaee533bf643952b8cfd9a80a14d8f1e
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2024-12-22 17:46:57 +0500

    Swagger

--- MinEndpoints/Program.cs
+++ MinEndpoints/Program.cs
@@ -5,13 +5,20 @@ using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSwaggerGen();
 builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
+
+
 var app = builder.Build();
 app.MapEndpoints();
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-
-
+app.UseHttpsRedirection();
 app.Run();
 
 
