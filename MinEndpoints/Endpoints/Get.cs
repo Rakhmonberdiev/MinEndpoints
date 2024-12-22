7ea24ba18bca78688c2fbcb0386c1ea668799368
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2024-12-22 18:51:53 +0500

    get endpoint

---
+++ MinEndpoints/Endpoints/Get.cs
@@ -0,0 +1,13 @@
+﻿using MinEndpoints.Abstractions;
+
+
+namespace MinEndpoints.Endpoints
+{
+    public class Get : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("get",()=>"Hello world");
+        }
+    }
+}
