1608f568cf25d28a430bbf2b6623fb58b249f731
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2024-12-22 17:33:16 +0500

    AddEndpoints method in EndpointExtensions

---
+++ MinEndpoints/Extensions/EndpointExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using MinEndpoints.Abstractions;
+using System.Reflection;
+
+namespace MinEndpoints.Extensions
+{
+    public static class EndpointExtensions
+    {
+        public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
+        {
+            ServiceDescriptor[] serviceDescriptors = assembly
+                .GetTypes()
+                .Where(type=>type is { IsAbstract:false, IsInterface: false } && type.IsAssignableTo(typeof(IEndpoint)))
+                .Select(type=>ServiceDescriptor.Transient(typeof(IEndpoint),type))
+                .ToArray();
+            services.TryAddEnumerable(serviceDescriptors);
+
+
+            return services;
+        }
+    }
+}
