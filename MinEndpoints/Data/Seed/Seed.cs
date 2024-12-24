7261bc8b2daf7eb31dc0ad3b2278d3fe5b699621
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2024-12-24 23:50:13 +0500

    SeedData

---
+++ MinEndpoints/Data/Seed/Seed.cs
@@ -0,0 +1,41 @@
+﻿using MinEndpoints.Data.Models;
+
+namespace MinEndpoints.Data.Seed
+{
+    public static class Seed
+    {
+        public static async Task SeedUsers(AppDbContext db)
+        {
+            using var transaction = db.Database.BeginTransaction();
+            try
+            {
+                if (db.Users.Any())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nusers exist\n");
+                    transaction.Rollback();
+                    return;
+                }
+                var users = new List<User>
+                {
+                    new User{Id = Guid.NewGuid(),UserName="John"},
+                    new User{Id = Guid.NewGuid(),UserName="Jane" },
+                    new User{Id = Guid.NewGuid(),UserName="Alex" },
+                    new User{Id = Guid.NewGuid(),UserName="Elena" },
+                    new User{Id = Guid.NewGuid(),UserName="Maxim" }
+                };
+                await db.Users.AddRangeAsync(users);
+                await db.SaveChangesAsync();
+                transaction.Commit();
+
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n"+e.Message+"\n");
+            }
+
+        }
+    }
+}
--- MinEndpoints/Extensions/MigrationExtensions.cs
+++ MinEndpoints/Extensions/MigrationExtensions.cs
@@ -1,17 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MinEndpoints.Data;
+using MinEndpoints.Data.Seed;
 
 namespace MinEndpoints.Extensions
 {
     public static class MigrationExtensions
     {
-        public static void ApplyMigrations(this IApplicationBuilder app)
+        public static async void ApplyMigrations(this IApplicationBuilder app)
         {
             using IServiceScope scope = app.ApplicationServices.CreateScope();
 
             using AppDbContext db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
             
             db.Database.Migrate();
+            await Seed.SeedUsers(db);
         }
     }
 }
